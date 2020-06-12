4e4bc8d9f0da83e7519d6a351488d5948a8ef95b
JavaProject039am <javaproject.03.9am@gmail.com>
2020-06-12 12:46:04 +0530

    HIS-300:Related changes

--- src/main/java/com/nit/SbWebAppApplication.java
+++ src/main/java/com/nit/SbWebAppApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbWebAppApplication {
+	private static final String REST_URL="https://www.equifax.com/getScores";
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbWebAppApplication.class, args);
