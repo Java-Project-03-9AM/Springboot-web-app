9dec3c67e2b4cb43a527384487070eb76fb7fd33
JavaProject039am <javaproject.03.9am@gmail.com>
2020-06-12 12:56:08 +0530

    HIS-301:Related changes

--- src/main/java/com/nit/SbWebAppApplication.java
+++ src/main/java/com/nit/SbWebAppApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbWebAppApplication {
+	private Map<String,Object> cache = new HashMap<String,Object>()
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbWebAppApplication.class, args);
  		int a = 10;
 	}
+
+	 //HIS-301 changes
+         public void loadDataToCache(){
+		//logic
+         }
 
 }
